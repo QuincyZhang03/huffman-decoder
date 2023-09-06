580a606919549fdc927d7644c311be600aecc4b4
QuincyZhang03 <1391070463@qq.com>
2023-09-06 18:02:11 +0800

    添加注释

--- HuffmanDecoder/HuffmanWindow.xaml.cs
+++ HuffmanDecoder/HuffmanWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -10,15 +9,15 @@ namespace HuffmanDecoder
 {
     public partial class HuffmanWindow : Window
     {
-        private const int NODE_SIZE = 40;
-        private const int VERTICAL_GAP = 70;
-        private const int FONT_SIZE = 15;
-        private TreeNode root;
+        private const int NODE_SIZE = 40; //结点圆大小
+        private const int VERTICAL_GAP = 70; //层间距
+        private const int FONT_SIZE = 15; //字体大小
+        private TreeNode root; //根节点
         private HuffmanWindow()
         {
             InitializeComponent();
         }
-        private void SetNodeStyle(Ellipse node, string toolTip)
+        private void SetNodeStyle(Ellipse node, string toolTip) //设置结点圆样式
         {
             node.Height = NODE_SIZE;
             node.Width = NODE_SIZE;
@@ -28,18 +27,21 @@ namespace HuffmanDecoder
             Label l = new Label();
             l.Content = toolTip;
             node.ToolTip = l;
-            ToolTipService.SetInitialShowDelay(node, 0);
+            ToolTipService.SetInitialShowDelay(node, 0); //即时显示悬停提示
         }
-        public HuffmanWindow(TreeNode root) : this()
+        public HuffmanWindow(TreeNode root) : this() //公开构造方法，需传入根节点以绘制树
         {
             this.root = root;
             DrawNode(root, 0, new Point(canvas.Width / 2, 10 + NODE_SIZE / 2));
         }
+        //绘制结点的方法，参数1为结点信息，参数2为结点层数，参数3为结点圆位置。该方法被递归调用，以先序遍历的方式绘制哈夫曼树。
         private void DrawNode(TreeNode node, int tier, Point pos)
         {
             Ellipse nodeElps = new Ellipse();
-            SetNodeStyle(nodeElps, (node.IsLeaf ? node.Value + "，对应哈夫曼编码为" + node.PathCode : "非叶子结点") + "\n权值为：" + node.Weight + "\n");
-            if (node.IsLeaf)
+            SetNodeStyle(nodeElps, 
+                (node.IsLeaf ? node.Value + "，对应哈夫曼编码为" + node.PathCode : "非叶子结点") 
+                + "\n权值为：" + node.Weight + "\n");
+            if (node.IsLeaf) //叶子结点，有码
             {
                 nodeElps.Fill = Brushes.Blue;
                 Label val = new Label();
@@ -54,8 +56,6 @@ namespace HuffmanDecoder
                 Canvas.SetZIndex(val, 1);
                 canvas.Children.Add(val);
             }
-
-
             Canvas.SetLeft(nodeElps, pos.X - NODE_SIZE / 2);
             Canvas.SetTop(nodeElps, pos.Y - NODE_SIZE / 2);
             canvas.Children.Add(nodeElps);
@@ -103,6 +103,7 @@ namespace HuffmanDecoder
                 DrawNode(node.RightChild, tier + 1, rightPos);//画右孩子
             }
         }
+        //鼠标滚轮滚动缩放功能
         private void scrollPane_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
         {
             const double SCALING_FACTOR = 0.5;//滑动一个鼠标滚轮单位的缩放量
@@ -113,14 +114,11 @@ namespace HuffmanDecoder
             }
             double ratio = alteredWidth / canvas.Width;
             canvas.Width = alteredWidth;
-            canvas.Children.Clear();
-            DrawNode(root, 0, new Point(canvas.Width / 2, 10 + NODE_SIZE / 2));
-            scrollPane.ScrollToHorizontalOffset(e.GetPosition(canvas).X * ratio - e.GetPosition(scrollPane).X);
-        }
 
-        private void canvas_ToolTipOpening(object sender, ToolTipEventArgs e)
-        {
-            MessageBox.Show("6");
+            canvas.Children.Clear();
+            DrawNode(root, 0, new Point(canvas.Width / 2, 10 + NODE_SIZE / 2));//重画哈夫曼树
+            scrollPane.ScrollToHorizontalOffset(e.GetPosition(canvas).X * ratio - e.GetPosition(scrollPane).X);
+            //滚动屏幕，保持鼠标所指向的位置在屏幕中的位置不变
         }
     }
 
--- HuffmanDecoder/MainWindow.xaml.cs
+++ HuffmanDecoder/MainWindow.xaml.cs
@@ -9,9 +9,9 @@ namespace HuffmanDecoder
 {
     public partial class MainWindow : Window
     {
-        private TreeNode treeRoot;
-        private bool codeShown = false;
-        Dictionary<string, char> codeMap = new Dictionary<string, char>();
+        private TreeNode treeRoot; //保存树的根节点
+        private bool codeShown = false; //用于显示/隐藏编码的扩展区的标记
+        Dictionary<string, char> codeMap = new Dictionary<string, char>(); //编码查询字典
 
         public MainWindow()
         {
@@ -23,19 +23,20 @@ namespace HuffmanDecoder
             }
             catch (Exception ex)
             {
-                MessageBox.Show("对不起，程序出现错误，错误信息如下：\n" + ex.Message, "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("对不起，程序出现错误，错误信息如下：\n" + ex.Message, 
+                    "错误", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             treeRoot = InitTree(data);
             StringBuilder sb = new StringBuilder();
             var keys = codeMap.Keys.ToList();
-            for (int i = 0; i < (keys.Count + 1) / 2; i++)
+            for (int i = 0; i < (keys.Count + 1) / 2; i++) //按列排列，即第一行为_N，第二行为AO，以此类推
             {
                 string key = keys[i];
                 char value = codeMap[keys[i]] == ' ' ? '_' : codeMap[keys[i]];
-                string entry = $"{key,15}={value,-2}";
+                string entry = $"{key,15}={value,-2}";//编码预留15字符，右对齐；明文预留2字符，左对齐
 
-                int oppositeIndex = i + keys.Count / 2 + 1;
-                if (oppositeIndex < keys.Count)
+                int oppositeIndex = i + keys.Count / 2 + 1; //本行第二列的文本信息
+                if (oppositeIndex < keys.Count) 
                 {
                     key = keys[oppositeIndex];
                     value = codeMap[keys[oppositeIndex]] == ' ' ? '_' : codeMap[keys[oppositeIndex]];
@@ -43,15 +44,17 @@ namespace HuffmanDecoder
                 }
                 sb.Append(entry + "\n");
             }
-            lbl_CodeInfo.Text = sb.ToString();
+            lbl_CodeInfo.Text = sb.ToString(); //显示在前端
         }
-        private Dictionary<char, int> ReadData(string path)
+
+        //读取csv文件
+        private Dictionary<char, int> ReadData(string path) 
         {
             Dictionary<char, int> data = new Dictionary<char, int>();
             StreamReader file = File.OpenText(path);
             while (file != null && !file.EndOfStream)
             {
-                string line = file.ReadLine();
+                string line = file.ReadLine(); //按行读取文件
                 char key = line[0];
                 int weight = Int32.Parse(line.Substring(2));
                 data.Add(key, weight);
@@ -59,6 +62,8 @@ namespace HuffmanDecoder
             file.Close();
             return data;
         }
+
+        //初始化树
         private TreeNode InitTree(Dictionary<char, int> data)
         {
             List<TreeNode> nodes = new List<TreeNode>();
@@ -69,13 +74,13 @@ namespace HuffmanDecoder
                 nodes.Add(leafNode);
                 leafNodes.Add(leafNode);
             }
-            while (nodes.Count > 1)
+            while (nodes.Count > 1)//反复选出权值最小的两个结点
             {
                 TreeNode left = PopNodeWithLeastWeight(nodes);
                 TreeNode right = PopNodeWithLeastWeight(nodes);
                 nodes.Add(TreeNode.CombineNode(left, right));
             }
-            PreOrderTraverse(nodes[0]);
+            AllocatePathCode(nodes[0]);
             foreach (TreeNode node in leafNodes)
             {
                 codeMap.Add(node.PathCode, node.Value);
@@ -86,6 +91,7 @@ namespace HuffmanDecoder
             }
             return null;
         }
+        //弹出最小权值结点
         private TreeNode PopNodeWithLeastWeight(List<TreeNode> list)
         {
             if (list.Count == 0)
@@ -105,23 +111,25 @@ namespace HuffmanDecoder
             list.Remove(nodeToPop);
             return nodeToPop;
         }
-        private void PreOrderTraverse(TreeNode root, string path = "")
+        //以先序遍历的方式为生成好的树分配结点编码
+        private void AllocatePathCode(TreeNode root, string path = "")
         {
             if (root == null)
             {
                 return;
             }
             root.PathCode = path;
-            PreOrderTraverse(root.LeftChild, path + "0");
-            PreOrderTraverse(root.RightChild, path + "1");
+            AllocatePathCode(root.LeftChild, path + "0");
+            AllocatePathCode(root.RightChild, path + "1");
         }
+        //显示哈夫曼树
         private void btn_ShowHuffmanTree_Click(object sender, RoutedEventArgs e)
         {
             HuffmanWindow huffmanWindow = new HuffmanWindow(treeRoot);
             huffmanWindow.Owner = this;
             huffmanWindow.Show();
         }
-
+        //译码：遇0往左，遇1往右，遇叶子结点则译出一位
         private void btn_Decode_Click(object sender, RoutedEventArgs e)
         {
             tb_DecodeResult.Text = "";
@@ -135,7 +143,8 @@ namespace HuffmanDecoder
                 {
                     if (sourcePos >= source.Length)
                     {
-                        MessageBox.Show($"在编码尾部发现非法码：{source.Substring(startIndex)}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                        MessageBox.Show($"在编码尾部发现非法码：{source.Substring(startIndex)}"
+                            , "错误", MessageBoxButton.OK, MessageBoxImage.Error);
                         return;
                     }
                     if (source[sourcePos] == '0')
@@ -148,22 +157,22 @@ namespace HuffmanDecoder
                     }
                     else
                     {
-                        MessageBox.Show($"发现非法字符：{source[sourcePos]}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                        MessageBox.Show($"发现非法字符：{source[sourcePos]}", "错误",
+                            MessageBoxButton.OK, MessageBoxImage.Error);
                         return;
                     }
-
                     sourcePos++;
                 }
                 tb_DecodeResult.Text += nodePtr.Value;
                 nodePtr = treeRoot;
             }
         }
-
+        //关于
         private void btn_About_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("版权所有：计算机科学与技术211班 张健\n日期：2023年9月", "关于程序");
         }
-
+        //回车快捷键
         private void tb_InputCode_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
             if (e.Key == System.Windows.Input.Key.Enter)
@@ -172,6 +181,7 @@ namespace HuffmanDecoder
             }
         }
 
+        //展开/折叠编码信息区
         private void btn_ShowCode_Click(object sender, RoutedEventArgs e)
         {
             if (codeShown)
@@ -188,15 +198,16 @@ namespace HuffmanDecoder
             }
         }
     }
+    //树结点类
     public class TreeNode
     {
         public bool IsLeaf { get; set; }
         public TreeNode Parent { get; set; }
         public TreeNode LeftChild { get; set; }
         public TreeNode RightChild { get; set; }
-        public char Value { get; }
-        public int Weight { get; }
-        public string PathCode { get; set; } = "";
+        public char Value { get; } //字符
+        public int Weight { get; } //权值
+        public string PathCode { get; set; } = ""; //储存编码
 
         public TreeNode(char value, int weight)
         {
@@ -209,7 +220,7 @@ namespace HuffmanDecoder
             Weight = weight;
             IsLeaf = false;
         }
-        public static TreeNode CombineNode(TreeNode left, TreeNode right)
+        public static TreeNode CombineNode(TreeNode left, TreeNode right) //静态方法，用于给定两个子结点生成新的父结点
         {
             TreeNode result = new TreeNode(left.Weight + right.Weight);
             result.LeftChild = left;
@@ -218,7 +229,7 @@ namespace HuffmanDecoder
             right.Parent = result;
             return result;
         }
-        public override string ToString()
+        public override string ToString() //用于调试，将结点转化为字符串
         {
             return "[" + (IsLeaf ? Value + "," : "") + Weight + "]";
         }
